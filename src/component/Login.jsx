b1896e41c2ee2dac13a73bdda24dc0e67443fac7
RuttvikKheni <rk.globaliasoft@gmail.com>
2021-02-08 06:05:00 +0530

    main

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -10,17 +10,11 @@ const Login = () => {
     const [data, setData] = useState("");
     const [user, setUser] = useState("");
     const [error, setError] = useState("");
-    const
+    const [empty, setEmpty] = useState(false);
+    const history = useHistory();
 
-    const onemailchange = (e) => {
-        setEmail(e);
-    }
-    const onpasswordchange = (e) => {
-        setPassword(e);
-    }
-
-    const onsubmit = (event) => {
-        event.preventDefault();
+    const onsubmit = (e) => {
+        e.preventDefault();
         console.log(email, password);
         if (email.match(/\S+@\S+\.\S+/) && email != "" && password != "") {
 
@@ -30,11 +24,11 @@ const Login = () => {
                     setData(res.data.data);
                     setUser(res.data.user);
                     setError(res.data.error);
+                    console.log(res.data);
                     if (status == 1) {
-                        console.log("login");
-                        return <Redirect to="/home" />
+                        history.push("/home");
                     } else {
-                        console.log("SOmeError");
+                        setError(res.data.err);
                     }
                 })
                 .catch((err) => {
@@ -42,7 +36,7 @@ const Login = () => {
                 });
 
         } else {
-            console.log("not Done");
+            setEmpty(true);
         }
     }
     return (
@@ -50,15 +44,21 @@ const Login = () => {
             <div className="col-10 mx-auto">
                 <h1 style={{ textAlign: "center" }}>Login</h1>
             </div>
+
             <div className="form col-8 mx-auto">
+                {
+                    empty ? <div class="alert alert-warning" role="alert">
+                        Fill All Records..
+                  </div> : null
+                }
                 <form method="post" onSubmit={(event) => onsubmit(event)}>
                     <div className="form-group m-3">
                         <label>Email address:</label>
-                        <input type="email" className="form-control" name="email" onBlur={event => onemailchange(event.target.value)} placeholder="Enter email" />
+                        <input type="email" className="form-control" name="email" onBlur={event => setEmail(event.target.value)} placeholder="Enter email" />
                     </div>
                     <div className="m-3 form-group">
                         <label>Password:</label>
-                        <input type="password" className="form-control" onBlur={event => onpasswordchange(event.target.value)} name="password" placeholder="Enter password" />
+                        <input type="password" className="form-control" onBlur={event => setPassword(event.target.value)} name="password" placeholder="Enter password" />
                     </div>
                     <button type="submit" className="m-3 btn btn-primary">Submit</button>
                 </form>
